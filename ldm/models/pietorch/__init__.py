542a908f808512d681b73c124de7a8bb94c17a14
anonymity-111 <221900399@smail.nju.edu.cn>
2025-08-04 11:32:28 +0800

    Add files via upload

---
+++ ldm/models/pietorch/__init__.py
@@ -0,0 +1,4 @@
+from .functional import blend, construct_green_function, blend_wide,blend_c
+from .cached_poisson_blender import CachedPoissonBlender
+from .functional_numpy import blend_numpy, construct_green_function_numpy, blend_wide_numpy, blend_dst_numpy
+# copy from https://github.com/matt-baugh/pytorch-poisson-image-editing
